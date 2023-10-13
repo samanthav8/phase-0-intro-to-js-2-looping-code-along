d045ef5cde40ac173f37547a41b74e4aaee77cc0
Samantha Villafana <samanthavillafana8@gmail.com>
2023-10-13 17:01:28 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+
+function writeCards(names) {
+    let messages = []; // Initialize an empty array to store the messages
+  
+    for (let i = 0; i < names.length; i++) {
+      let message = `Thank you, ${names[i]}, for the wonderful surprise gift!`;
+      messages.push(message); // Push each message into the array
+    }
+  
+    return messages; // Return the array of messages
+  }
+
+  function countDown(num) {
+    while (num >= 0) {
+      console.log(num);
+      num--;
+    }
+  }
+  
+  countDown(10);
